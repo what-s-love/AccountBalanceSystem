9ee8bbb57c1f015bc2bf1702daf30b2541ab0a7f
what-s-love <barabanabanan@gmail.com>
2025-06-03 20:58:21 +0300

    Added environment file. Added Liquibase settings and initial migrations. Added datasource settings.

---
+++ src/main/resources/db/changelog/initial/002-insert-test-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO CURRENCIES (NAME, CODE, RATE)
+VALUES ('Доллар США', 'USD', 1.0),
+       ('Евро', 'EUR', 1.14),
+       ('Белорусский рубль', 'BYN', 0.31),
+       ('Российский рубль', 'RUB', 0.013);
+
+INSERT INTO BALANCES (NAME, VALUE)
+VALUES ('test', 0.0);
