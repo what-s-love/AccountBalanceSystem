b100a541fd94e7e75a704a57d4b5759533103a28
what-s-love <barabanabanan@gmail.com>
2025-06-05 13:46:23 +0400

    Enabled scheduled update of currencies

--- src/main/java/ge/accbalsystem/AccBalSystemApplication.java
+++ src/main/java/ge/accbalsystem/AccBalSystemApplication.java
@@ -2,13 +2,14 @@ package ge.accbalsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AccBalSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccBalSystemApplication.class, args);
 	}
 
-	//ToDo Сделать планированное обновление курса валют со стор. API
 }
